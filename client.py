a252607840a88fa1cd74bf3c3541b2fdd1731923
unknown <scaifei@CAD36.ad.mee.tcd.ie>
2017-02-21 22:30:54 +0000

    Add error checking

--- client.py
+++ client.py
@@ -4,7 +4,18 @@ import CRC
 import pickle
 from socket import *
 
+# A function that will alter it's input integer 1/10 times,
+# and return the input unchanged 9/10 times
 
+def gremlin(input):
+    chance = random.random()
+    chance = int(chance*10)
+    if chance == 5:
+        return input + 4
+    else:
+        return input
+        
+        
 class Packet:
    'Class that defines a packet'
    #empCount = 0
@@ -19,43 +30,52 @@ class Packet:
      print("\n")
     
 
-
-
-
 #Generate a file of 1024 random characters
-	
+    
 gen_file = open("string.txt", "w")
 string = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(1024)])
 gen_file.write(string)
 gen_file.close()
+
+
+#Make an array to store packet
+stored_packets = []
 
 sequence_no = 0
 serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-	
+    
 with open("string.txt", "rb") as f:  #While file is open
     while True:
         payload = f.read(8)          #Read in 8 bytes from file
         if not payload:            
             break  
         newPacket = Packet(sequence_no, payload) 
-
         #pickle the packet into a byte array
         pickled_packet = pickle.dumps(newPacket)
-		#send the packet
+        #save the packet in case in needs to be resent
+        stored_packets.append(pickled_packet)
+        #send the packet
         clientSocket.send(pickled_packet)
         acknowledged = 0
         while acknowledged != 1:
+            #Receive reply from server
             ack_string = clientSocket.recv(1024)
+            #If the reply is the preset acknowledgement string, the packet has been acknowledged
             if ack_string == b"666":
                 acknowledged = 1
-                print("ack received \n")
+            #Otherwise, the reply is the sequence number of a packet to be resent
+            else:
+                ack_int = int(ack_string.decode())
+                print(ack_int)
+                clientSocket.send(stored_packets[ack_int])
+        print("acknowledge received \n")
         Packet.displayPacket(newPacket)
         sequence_no+=1
-		
-#Send a special packet that symbolises the end of the file		
+        
+#Send a special packet that symbolises the end of the file        
 end_frame_packet = Packet(-1, 0)
 end_pickled = pickle.dumps(end_frame_packet)
 clientSocket.send(end_pickled)
--- server.py
+++ server.py
@@ -1,8 +1,18 @@
 import pickle
+import random
 import CRC
 from socket import *
 
-
+def gremlin(input):
+    chance = random.randint(1, 10)
+    print("random function returned: ")
+    print(str(chance))
+    if chance == 5:
+        return input + 4
+    else:
+        return input
+    
+    
 class Packet:
    'Class that defines a packet'
 
@@ -16,39 +26,45 @@ class Packet:
      print("\n")
     
 
-
-	
-	
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print ('The server is ready to receive')
 connectionSocket, addr = serverSocket.accept()
-	
+    
 isEnded = 0
-
+last_sequence = -1
 while isEnded == 0:
      
-	#listen for incoming data
+    #listen for incoming data
     packet = connectionSocket.recv(1024)
-	#load bytes object back into Packet object using pickle
+    #load bytes object back into Packet object using pickle
     recv_packet = pickle.loads(packet)
-	#Print the string of the entire packet
+    #Apply gremlin function to possibly mess up sequence number
+    recv_packet.sequence_number = gremlin(recv_packet.sequence_number)  
+    #Print the string of the entire packet
     Packet.displayPacket(recv_packet)
-	
-	#check the checksum
-    checksum = CRC.crc(str(recv_packet.checksum))
-    if checksum == recv_packet.payload:
+    #check the checksum
+    checksum = CRC.crc(str(recv_packet.payload))
+    #check if the packet is in sequence, and if the checksum is valid
+    if (recv_packet.sequence_number != (last_sequence + 1)) or (checksum != recv_packet.checksum):
+        #send back the index of the missing packet if not
+        connectionSocket.send(str(last_sequence + 1).encode())
+        
+    #if the sequence is correct...
+    else:
         #send an acknowledgement
         connectionSocket.send(b"666")
         print("Acknowledgement sent \n")
-    if recv_packet.sequence_number == -1:
-	    isEnded = 1
-connectionSocket.close()	
-	
-	
-	
-	
-	
-	
+        if recv_packet.sequence_number == -1:
+            isEnded = 1
+        
+        
+connectionSocket.close()    
+    
+    
+    
+    
+    
+    

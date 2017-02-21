3a65fd9d9010932e9dc5b10df287eac0d05c24f1
Iarla Brendan Scaife <scaifei@itserv.scss.tcd.ie>
2017-02-21 15:54:59 +0000

    Add working object sending

--- client.py
+++ client.py
@@ -1,67 +1,28 @@
 import random
 import string
+import CRC
+import pickle
+from socket import *
+
 
 class Packet:
    'Class that defines a packet'
    #empCount = 0
 
-   def __init__(sequence_number, payload):
+   def __init__(self, sequence_number, payload):
       self.sequence_number = sequence_number
       self.payload = payload
-      self.checksum = crc(payload)
-      self.packet = str(self.sequence_number) + payload + checksum
+      self.checksum = CRC.crc(str(payload))
+      self.packet_string = str(self.sequence_number) + str(self.payload) + str(self.checksum)
    def displayPacket(self):
-     print ("Packet:  %d" % self.packet)
+     print (self.packet_string)
+     print("\n")
+    
 
 
 
 
-#CRC Algorithm, ported from C by Serge Ballesta
-
-POLYNOMIAL = 0x1021
-PRESET = 0
-
-def _initial(c):
-    crc = 0
-    c = c << 8
-    for j in range(8):
-        if (crc ^ c) & 0x8000:
-            crc = (crc << 1) ^ POLYNOMIAL
-        else:
-            crc = crc << 1
-        c = c << 1
-    return crc
-
-_tab = [ _initial(i) for i in range(256) ]
-
-def _update_crc(crc, c):
-    cc = 0xff & c
-
-    tmp = (crc >> 8) ^ cc
-    crc = (crc << 8) ^ _tab[tmp & 0xff]
-    crc = crc & 0xffff
-    print (crc)
-
-    return crc
-
-def crc(str):
-    crc = PRESET
-    for c in str:
-        crc = _update_crc(crc, ord(c))
-    return crc
-
-def crcb(*i):
-
-    crc = PRESET
-    for c in i:
-        crc = _update_crc(crc, c)
-    return crc
-	
-	
-	
-	
-	
-	
+#Generate a file of 1024 random characters
 	
 gen_file = open("string.txt", "w")
 string = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(1024)])
@@ -69,31 +30,34 @@ gen_file.write(string)
 gen_file.close()
 
 sequence_no = 0
-
-with open("string.txt", "rb") as f:
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+	
+with open("string.txt", "rb") as f:  #While file is open
     while True:
-        payload = f.read(8)
-        if not payload:
-            break
-        newPacket = Packet(payload, sequence_no)
-		Packet.displayPacket(newPacket)
-        sequence_no++
+        payload = f.read(8)          #Read in 8 bytes from file
+        if not payload:            
+            break  
+        newPacket = Packet(sequence_no, payload) 
 
-
+        #pickle the packet into a byte array
+        pickled_packet = pickle.dumps(newPacket)
+		#send the packet
+        clientSocket.send(pickled_packet)
+        acknowledged = 0
+        while acknowledged != 1:
+            ack_string = clientSocket.recv(1024)
+            if ack_string == b"666":
+                acknowledged = 1
+                print("ack received \n")
+        Packet.displayPacket(newPacket)
+        sequence_no+=1
 		
-
-from socket import *
-while 1:
-
-	serverName = 'localhost'
-	serverPort = 12000
-	clientSocket = socket(AF_INET, SOCK_STREAM)
-	clientSocket.connect((serverName,serverPort))
-
-	sentence = input('Enter lowercase sentence:').encode()
-	clientSocket.send(sentence)
-
-	modifiedSentence = clientSocket.recv(1024)
-	print ('Received From Server:', modifiedSentence)
-
+#Send a special packet that symbolises the end of the file		
+end_frame_packet = Packet(-1, 0)
+end_pickled = pickle.dumps(end_frame_packet)
+clientSocket.send(end_pickled)
+#Close the socket
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,44 +1,22 @@
+import pickle
+import CRC
 from socket import *
 
-#CRC Algorithm, ported from C by Serge Ballesta
 
-POLYNOMIAL = 0x1021
-PRESET = 0
+class Packet:
+   'Class that defines a packet'
 
-def _initial(c):
-    crc = 0
-    c = c << 8
-    for j in range(8):
-        if (crc ^ c) & 0x8000:
-            crc = (crc << 1) ^ POLYNOMIAL
-        else:
-            crc = crc << 1
-        c = c << 1
-    return crc
+   def __init__(self, sequence_number, payload):
+      self.sequence_number = sequence_number
+      self.payload = payload
+      self.checksum = crc(str(payload))
+      self.packet_string = str(self.sequence_number) + str(self.payload) + str(self.checksum)
+   def displayPacket(self):
+     print (self.packet_string)
+     print("\n")
+    
 
-_tab = [ _initial(i) for i in range(256) ]
 
-def _update_crc(crc, c):
-    cc = 0xff & c
-
-    tmp = (crc >> 8) ^ cc
-    crc = (crc << 8) ^ _tab[tmp & 0xff]
-    crc = crc & 0xffff
-    print (crc)
-
-    return crc
-
-def crc(str):
-    crc = PRESET
-    for c in str:
-        crc = _update_crc(crc, ord(c))
-    return crc
-
-def crcb(*i):
-    crc = PRESET
-    for c in i:
-        crc = _update_crc(crc, c)
-    return crc
 	
 	
 serverPort = 12000
@@ -46,10 +24,31 @@ serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 print ('The server is ready to receive')
+connectionSocket, addr = serverSocket.accept()
+	
+isEnded = 0
 
-while 1:
-	connectionSocket, addr = serverSocket.accept()
-	sentence = connectionSocket.recv(1024)
-	capitalizedSentence = sentence.upper()
-	connectionSocket.send(capitalizedSentence)
-	connectionSocket.close()
+while isEnded == 0:
+     
+	#listen for incoming data
+    packet = connectionSocket.recv(1024)
+	#load bytes object back into Packet object using pickle
+    recv_packet = pickle.loads(packet)
+	#Print the string of the entire packet
+    Packet.displayPacket(recv_packet)
+	
+	#check the checksum
+    checksum = CRC.crc(str(recv_packet.checksum))
+    if checksum == recv_packet.payload:
+        #send an acknowledgement
+        connectionSocket.send(b"666")
+        print("Acknowledgement sent \n")
+    if recv_packet.sequence_number == -1:
+	    isEnded = 1
+connectionSocket.close()	
+	
+	
+	
+	
+	
+	
